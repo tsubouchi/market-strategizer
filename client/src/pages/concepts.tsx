31950be688cd6617de55656d7bd991aa8326ad0b
bongin <37717636-bongin@users.noreply.replit.com>
2025-01-19 09:41:59 +0000

    Agent query: ページの左右の余白は適切になりましたか？
    Update page layouts with consistent horizontal padding.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/c7112613-c3ed-496b-9162-5a474af12084/740afb40-baf6-4bdc-ad7b-85d60f7c2903.jpg

--- client/src/pages/concepts.tsx
+++ client/src/pages/concepts.tsx
@@ -20,7 +20,7 @@ export default function Concepts() {
   }
 
   return (
-    <div className="container mx-auto py-8">
+    <div className="container mx-auto py-8 px-4 md:px-6">
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-4xl font-bold">商品コンセプト一覧</h1>
         <Dialog>
